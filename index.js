19123edb685e54355006ff07b9658c6407ba1c42
Austin Holeman <austinholeman@gmail.com>
2022-01-26 12:40:34 -0800

    commit tree 6d579ca10b2e4354ebca7c2f457fb4dbf787d3ec

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+let result;
+   if (string === string.toLowerCase()) {
+        result = "I can\'t hear you!"
+   } else if (string === string.toUpperCase()) {
+        result = "YES INDEED!"
+   } else {
+        result = "I would love to!"
+   }
+   return result;
+}
